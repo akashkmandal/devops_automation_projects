d1b8282b01cadf3b54b2c870f4b3ce71ec8ee478
akashkmandal <47422269+akashkmandal@users.noreply.github.com>
2023-02-15 09:50:52 +0530

    Update 30_days_pred.py

--- 30_days_pred.py
+++ 30_days_pred.py
@@ -27,6 +27,7 @@ def create_dataset(data, look_back=1):
     for i in range(len(data) - look_back - 1):
         x.append(data[i:(i + look_back), 0])
         y.append(data[i + look_back, 0])
+    #return np.array(x), np.array(y).reshape(-1, 1)
     return np.array(x), np.array(y)
 
 look_back = 5
@@ -57,12 +58,19 @@ y_pred = scaler.inverse_transform(model.predict(x_test))
 # Predict the price for the next week
 # Predict the price for the next week
 last_predicted = data[-look_back:].reshape(1, look_back, 1)
+predictions = []
 for i in range(7):
-    next_day_price = model.predict(last_predicted)[0, 0]
-    last_predicted = np.append(last_predicted[:,1:,:], [[next_day_price]], axis=1)
+    next_day_price = model.predict(last_predicted)
+    last_predicted = np.append(last_predicted[:,1:,:], next_day_price.reshape(1, 1, 1), axis=1)
+    predictions.append(next_day_price[0, 0])
     date = pd.to_datetime(df['Date'].iloc[-1]) + pd.DateOffset(days=1)
     new_row = {'Date': date.date(), 'Close': scaler.inverse_transform([[next_day_price]])[0][0]}
     df = df.append(new_row, ignore_index=True)
+
+# Add predicted price to DataFrame
+dates = pd.date_range(start=df['Date'].iloc[-1], periods=7, freq='D')[1:]
+new_rows = pd.DataFrame({'Date': dates, 'Close': scaler.inverse_transform(np.array(predictions).reshape(-1, 1)).flatten()})
+df = pd.concat([df, new_rows], ignore_index=True)
 
 # Convert Date column to string
 df['Date'] = pd.to_datetime(df['Date'])
